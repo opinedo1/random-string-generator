26007ce844f4904ded6a6e92e3b2543a7a926bf4
Oscar Pinedo <opinedo19@gmail.com>
2021-05-16 02:54:11 -0700

    cleaned up code and added comments

--- random_word_generator_app/urls.py
+++ random_word_generator_app/urls.py
@@ -4,9 +4,5 @@ urlpatterns = [
     path('', views.random),
     path('random_word', views.random),
     path('random_word/reset_counter', views.reset),
-    path('test_redirect', views.random_word)
-    # path('submission', views.submission),
-    # path('thanks', views.thanks)
-    # this is a parameter passed in view.py
-    # path('<str:param_name>', views.name)
+    path('random_word_redirect', views.random_word)
 ]
--- random_word_generator_app/views.py
+++ random_word_generator_app/views.py
@@ -3,25 +3,30 @@ from django.shortcuts import render, redirect
 from django.utils.crypto import get_random_string
 
 # Create your views here.
+
+
+# logic for generating a random string and keeping a count
+# uses redirect to avoind resubmission alert
 def random(request):
     # checks to see that count isnt stored already then we increment if it is
     if 'count' not in request.session:
         request.session['count'] = 0
     request.session['count'] += 1
-    #define our random string, then store it in session variable
+    # define our random string, then store it in session variable
     randomString = get_random_string(length=14)
     request.session['str'] = randomString
     print("Count: " + str(request.session['count']) + "String: " + request.session['str'])
 
-    return redirect('/test_redirect')
+    return redirect('/random_word_redirect')
 
+# renders our html template
 def random_word(request):
-    print(request.session)
     return render(request, 'random_word.html')
 
+
+# resets the count for random strings
 def reset(request):
     if request.method == 'GET':
         return redirect('/')
-    
     request.session.flush()
     return redirect("/random_word")
